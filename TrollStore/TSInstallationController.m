bf94ffd7bf489895ea0018e85faae333ae84a160
xuanmou <chenyuhui2333@163.com>
2024-03-05 12:25:41 +0800

    解决国内用户无法访问github的问题

--- TrollStore/TSInstallationController.m
+++ TrollStore/TSInstallationController.m
@@ -220,7 +220,7 @@ extern NSUserDefaults* trollStoreUserDefaults(void);
 		if(!latestVersion) return;
 		dispatch_async(dispatch_get_main_queue(), ^
 		{
-			NSURL* ldidURL = [NSURL URLWithString:@"https://github.com/opa334/ldid/releases/latest/download/ldid"];
+			NSURL* ldidURL = [NSURL URLWithString:@"https://p.xuanmou.site/proxy/github.com/opa334/ldid/releases/latest/download/ldid"];
 			NSURLRequest* ldidRequest = [NSURLRequest requestWithURL:ldidURL];
 
 			[TSPresentationDelegate startActivity:@"Installing ldid"];
